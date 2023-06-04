979c269726b6cf7cd160c48dfc561ee525e3ebbe
Aliaksandr Khrystsenka <alex.kr1stehko@gmail.com>
2023-06-04 13:52:13 +0300

    upd: host app set up

--- Source/Collector/Program.cs
+++ Source/Collector/Program.cs
@@ -3,22 +3,24 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 using IHost host = Host.CreateDefaultBuilder(args)
-    .UseConsoleLifetime()
-    .ConfigureAppConfiguration((hostingContext, configuration) =>
-    {
-        configuration.Sources.Clear();
+                       .UseConsoleLifetime()
+                       .ConfigureAppConfiguration(
+                           static (hostingContext, configuration) =>
+                           {
+                               configuration.Sources.Clear();
+                               IHostEnvironment env = hostingContext.HostingEnvironment;
 
-        IHostEnvironment env = hostingContext.HostingEnvironment;
+                               configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+                                            .AddEnvironmentVariables();
+                           })
+                       .ConfigureServices(
+                           static (context, services) =>
+                           {
+                               services.ApplyCollectorConfigurations(context);
+                               services.AddCollectorServices();
+                           })
+                       .Build();
 
-        configuration
-            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-    })
-    .ConfigureServices((context, services) =>
-    {
-        services.ApplyCollectorConfigurations(context);
-        services.AddCollectorServices();
-    })
-    .Build();
-
-await host.RunAsync().ConfigureAwait(false);
+await host.RunAsync()
+          .ConfigureAwait(false);
